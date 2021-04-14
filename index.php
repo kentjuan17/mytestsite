67bc70e1d2aef921b6d59a4c41d85dba84dc6ec4
KinJooBun <kentjuan17@gmail.com>
2021-04-14 17:19:30 +0800

    Added CSS Styles - NAVbar and Banner
    from tutorials again

--- index.php
+++ index.php
@@ -12,41 +12,61 @@
     <body id="homepage" class="mainbody">
         <?php include "header.html"; ?>
 
-        <i class="fas fa-user-astronaut"></i>
-
-        <h1>Hello World</h1>
-    <!-- WHAT IS YOUR NAME??
-        <h2>What is your name?</h2>
-    
-        <p>
-            <input type="text" name="name" id="name" class="name" placeholder="Enter your name" />
-        </p>
-        <p>
-            <input type="text" name="age" id="age" class="age" placeholder="Enter your age" />
-            <input type="submit" name="submit" id="submit" class="btnName" placeholder="Click to submit" onClick="alertDetails()" />
-        </p>
-    -->
-        <h2>Enter Amount</h2>
-        <p>
-            <input type="text" name="price1" id="price1" class="price" placeholder="Enter amount" />
-        </p>
-        <p>
-            <input type="text" name="price2" id="price2" class="price" placeholder="Enter amount" />
-        </p>
-        <p>
-            <input type="text" name="price3" id="price3" class="price" placeholder="Enter amount" />
-        </p>
-        <p>
-            <input type="text" name="price4" id="price4" class="price" placeholder="Enter amount" />
-        </p>
-        <p>
-            <input type="submit" name="submit" id="submit" class="btnName" value="Click Me!"/>
-        </p>
+        <main>
         
-        <p>
-            <input type="text" name="total" id="total" class="total" placeholder="Total amount will be displayed here" />
-        </p>
-        
+            <section class="banner">    
+                <h1 class="h1-sticky">Welcome</h1>
+            </section>
+                
+                    <!-- WHAT IS YOUR NAME??
+                        <h2>What is your name?</h2>
+                    
+                        <p>
+                            <input type="text" name="name" id="name" class="name" placeholder="Enter your name" />
+                        </p>
+                        <p>
+                            <input type="text" name="age" id="age" class="age" placeholder="Enter your age" />
+                            <input type="submit" name="submit" id="submit" class="btnName" placeholder="Click to submit" onClick="alertDetails()" />
+                        </p>
+                    -->
+            <section class="display-sum-section">
+                <div class="calculate-sum">
+                        <h2>Please enter the amount in each box to get the sum</h2> </div>
+                        <p>
+                            <input type="text" name="price1" id="price1" class="price" placeholder="Enter amount" />
+                        </p>
+                        <p>
+                            <input type="text" name="price2" id="price2" class="price" placeholder="Enter amount" />
+                        </p>
+                        <p>
+                            <input type="text" name="price3" id="price3" class="price" placeholder="Enter amount" />
+                        </p>
+                        <p>
+                            <input type="text" name="price4" id="price4" class="price" placeholder="Enter amount" />
+                        </p>
+                </div>
+                <div class="sum">
+                            <p>
+                                <input type="submit" name="submit" id="submit" class="btnName" value="Click Me!"/>
+                            </p>
+                            <p>
+                                <input type="text" name="total" id="total" class="total" placeholder="Total amount will be displayed here" />
+                            </p>
+                </div>
+                <div>
+                    <h3>Sticky Navigation Bar Example</h3>
+                    <p>The navbar will <strong>stick</strong> to the top when you reach its scroll position.</p>
+                    <p><strong>Note:</strong> Internet Explorer do not support sticky positioning and Safari requires a -webkit- prefix.</p>
+                    <p>Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+                    <p>Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+                    <p>Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+                    <p>Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+                    <p>Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+                    <p>Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+                    <p>Some text to enable scrolling. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+                </div>
+            </section>
+        </main>
         <?php include "footer.html"; ?>
 
     </body>
