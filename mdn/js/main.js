1b4bd4c03d3255f50698d9988d39ce0a06f44ba5
KinJooBun <kentjuan17@gmail.com>
2021-04-28 19:41:17 +0800

    mdn revise style

---
+++ mdn/js/main.js
@@ -0,0 +1,45 @@
+// console.log("Hello world");
+
+// let Name = 'Kent';
+// let age = '32';
+
+// console.log(`My name is ${Name}, and I am ${age} years old.`);
+
+// let dog = {
+//     name: 'Snow White',
+//     breed: 'Japanese Spitz'
+// };
+
+// console.log(dog.name);
+
+
+// // Add your code here
+// let myName = 'Kent';
+// let myAge = 32;
+
+// // Don't edit the code below here!
+
+// section.innerHTML = ' ';
+// let para1 = document.createElement('p');
+// para1.textContent = myName;
+// let para2 = document.createElement('p');
+// para2.textContent = myAge;
+// section.appendChild(para1);
+// section.appendChild(para2);
+
+
+let myInt = 5;
+let myFloat = 6.667;
+myInt;
+myFloat;
+
+typeof myInt;
+typeof myFloat;
+
+
+console.log(`${myInt} \n +  ${myFloat}`);
+
+let lotsOfDecimal = 1.766584958675746364;
+console.log(lotsOfDecimal);
+let twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log(twoDecimalPlaces);
