6d247fcae77330a6e39adbb4e35f60b089065a61
KinJooBun <kentjuan17@gmail.com>
2021-05-07 23:35:57 +0800

    jsprojects - project reviews

--- jsprojects/reviewproject/app.js
+++ jsprojects/reviewproject/app.js
@@ -37,3 +37,60 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+
+// select items
+const img = document.querySelector('#person-img');
+const author = document.querySelector('#author');
+const job = document.querySelector('#job');
+const info = document.querySelector('#info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// starting item
+let current = 0;
+
+//load initial item
+window.addEventListener('DOMContentLoaded', () => {
+  showPerson(current);
+});
+
+
+//function to show person based on item
+const showPerson = person => {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', () => {
+  current++;
+  if (current > reviews.length - 1) {
+    current = 0
+  }
+  showPerson(current);
+})
+
+// show previouse person
+prevBtn.addEventListener('click', () => {
+  current--;
+  if (current < 0) {
+    current = reviews.length -1
+  }
+  showPerson(current);
+})
+
+
+// show random person - button
+let random = num => Math.floor(Math.random() * num);
+
+randomBtn.addEventListener('click', () => {
+  current = random(reviews.length);
+  showPerson(current);
+})
+
