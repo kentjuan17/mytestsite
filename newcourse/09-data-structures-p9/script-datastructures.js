f8e400de5202c1241f476bbbe615626fc1d28419
KinJooBun <kentjuan17@gmail.com>
2021-06-18 17:56:04 +0800

    added code challenge

--- newcourse/09-data-structures-p9/script-datastructures.js
+++ newcourse/09-data-structures-p9/script-datastructures.js
@@ -344,49 +344,101 @@ const restaurant = {
 // console.log(staffUnique);
 
 // MAPS - data structure to map values to keys
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-rest.set(2, 'Lisbon, Portugal');
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// rest.set(2, 'Lisbon, Portugal');
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed');
 
-// console.log(rest.get('name'));
-// console.log(rest.get(true));
-// console.log(rest.get(1));
+// // console.log(rest.get('name'));
+// // console.log(rest.get(true));
+// // console.log(rest.get(1));
 
-const time = 12;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 12;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-// console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear();
-const arr = [1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
-// console.log(rest);
-// console.log(rest.size);
+// // console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// // console.log(rest);
+// // console.log(rest.size);
 
-// console.log(rest.get(arr));
+// // console.log(rest.get(arr));
 
-const question = new Map([
-  ['question', 'What is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct 👌'],
-  [false, 'Try again!'],
-]);
-console.log(question);
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct 👌'],
+//   [false, 'Try again!'],
+// ]);
+// console.log(question);
 
-// Convert objects to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
+// // Convert objects to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
 
-console.log(hoursMap);
+// console.log(hoursMap);
+
+// // Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
+// console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+// console.log([...question]);
+// console.log([question.entries()]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
+
+const airline = 'AC Air Canada';
+const plane = 'A330';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+
+console.log('B737'[0]);
+console.log('B737'[2]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('a'));
+console.log(airline.lastIndexOf('a'));
+console.log(airline.indexOf('Canada'));
+
+console.log(airline.slice(3));
+console.log(airline.slice(3, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log();
+
+// const title = document.querySelector('h1');
+
+// title.addEventListener('click', () => {
+//   if (title.style.color === 'blue') {
+//     title.style.color = 'red';
+//   } else {
+//     title.style.color = 'blue';
+//   }
+
+//   // const titleStyle = title.style.color === 'blue' ? 'red' : 'blue';
+//   // title.style.color === 'blue' ? 'red' : 'blue';
+// });
