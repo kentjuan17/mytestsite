8847bd1d6a1154024b677d4f5a85defab6098d2a
KinJooBun <kentjuan17@gmail.com>
2021-07-12 00:42:21 +0800

    Update codechallenge3.js

--- newcourse/11-arrays-bankist-p11/codechallenge3.js
+++ newcourse/11-arrays-bankist-p11/codechallenge3.js
@@ -11,24 +11,24 @@ Test data:
 */
 
 // FROM CODE CHALLENGE #2
-const calcAverageHumanAge = function (ages) {
-  //   using Map Method
-  const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
-  console.log(humanAge);
+// const calcAverageHumanAge = function (ages) {
+//   //   using Map Method
+//   const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   console.log(humanAge);
 
-  // using Filter method
-  const adultDogs = humanAge.filter(age => age >= 18);
-  console.log(adultDogs);
+//   // using Filter method
+//   const adultDogs = humanAge.filter(age => age >= 18);
+//   console.log(adultDogs);
 
-  // using Reduce Method
-  const averageAge =
-    adultDogs.reduce((acc, cur) => acc + cur, 0) / adultDogs.length;
-  console.log(averageAge);
-};
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+//   // using Reduce Method
+//   const averageAge =
+//     adultDogs.reduce((acc, cur) => acc + cur, 0) / adultDogs.length;
+//   console.log(averageAge);
+// };
+// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
-console.log('---- CHAINING ----');
+// console.log('---- CHAINING ----');
 
 // Arrow function and Using chaining
 const calcAverageHumanAgeChain = ages => {
